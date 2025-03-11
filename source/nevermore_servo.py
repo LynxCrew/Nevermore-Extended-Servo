7bd0e28cb8481a75302b0ef079ea12073c0e882e
Zeanon <thezeanon@gmail.com>
2025-03-11 13:47:51 +0100

    Update nevermore_servo.py

--- source/nevermore_servo.py
+++ source/nevermore_servo.py
@@ -359,9 +359,9 @@ class ControlBangBang:
         elif self.heating == self.reverse and temp <= target_temp - self.max_delta:
             self.heating = not self.reverse
         if self.heating:
-            return self.min_percent
+            return 0.0
         else:
-            return self.max_percent
+            return 1.0
 
     def check_busy(self, eventtime, smoothed_temp, target_temp):
         return smoothed_temp < target_temp - self.max_delta
@@ -585,15 +585,9 @@ class ControlPID:
         bounded_co = max(0.0, min(1.0, co))
         try:
             if not self.reverse:
-                return (
-                    max(0.0, bounded_co) * (self.max_percent - self.min_percent)
-                    + self.min_percent
-                )
+                return max(0.0, bounded_co)
             else:
-                return (
-                    max(0.0, 1.0 - bounded_co) * (self.max_percent - self.min_percent)
-                    + self.min_percent
-                )
+                return max(0.0, 1.0 - bounded_co)
         finally:
             # Store state for next measurement
             self.prev_temp = temp
